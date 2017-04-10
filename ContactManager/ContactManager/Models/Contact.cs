79504f6bc5a75de8d9829fc1473126212922e057
diegotavarez <tavares.diego@outlook.com>
2017-04-10 09:33:13 -0300

    ContactManager Sample

---
+++ ContactManager/ContactManager/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public Contact(int Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ ContactManager/ContactManager/Services/ContactRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContactManager.Models;
+
+namespace ContactManager.Services
+{
+    public class ContactRepository
+    {
+        private const string CacheKey = "ContactStore";
+
+        public ContactRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var contacts = new Contact[]
+                    {
+                        new Contact(1,"Glenn Block"),
+                        new Contact(2,"Dan Roth")
+                    };
+
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+        }
+
+        public Contact[] GetAllContacts()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                return (Contact[])ctx.Cache[CacheKey];
+            }
+
+            return new Contact[]{ new Contact(0,"Placeholder") };
+        }
+
+        public bool SaveContact(Contact contact)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Contact[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(contact);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
